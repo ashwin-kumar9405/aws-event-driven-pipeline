4510f8b875f4f386f76bf12756d525cfe2f5c4b8
ashwin-kumar9405 <ashwinkumar092005@gmail.com>
2026-01-01 23:03:54 +0530

    Add IAM role and source code hash to Lambda function
    Added IAM role and source code hash for the Lambda function.

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,5 +2,9 @@ resource "aws_lambda_function" "ingestion_lambda" {
   function_name = "data-ingestion-lambda"
   runtime       = "python3.9"
   handler       = "lambda_function.lambda_handler"
-  filename      = "ingestion.zip"
+
+  role = aws_iam_role.lambda_role.arn
+
+  filename         = "ingestion.zip"
+  source_code_hash = filebase64sha256("ingestion.zip")
 }
