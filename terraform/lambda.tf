58d341bb0690d3678c3e580c7f584a16c90a37a2
ashwin-kumar9405 <ashwinkumar092005@gmail.com>
2026-01-01 23:15:16 +0530

    Remove Lambda deployment from Terraform
    Added comments about manual deployment and role management.

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,10 +1,2 @@
-resource "aws_lambda_function" "ingestion_lambda" {
-  function_name = "data-ingestion-lambda"
-  runtime       = "python3.9"
-  handler       = "lambda_function.lambda_handler"
-
-  role = aws_iam_role.lambda_role.arn
-
-  filename         = "ingestion.zip"
-  source_code_hash = filebase64sha256("ingestion.zip")
-}
+# Lambda function code is deployed manually / conceptually
+# Infrastructure for Lambda execution role is managed via Terraform
